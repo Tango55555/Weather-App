bb50630ff526bfecb9c4325e967f23fff9ad4167
Tango55555 <tangocar363@gmail.com>
2025-08-03 07:01:00 +0100

    Added css styles and design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 
+const api = {
+  key: "4aa4daefda3a270ad92d41458e8b35bd",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+          
+        </div>
+      </main>
     </div>
   );
 }
